cb2fab811ef2a57a2cb214c42918c8db6b3ade91
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-10-04 12:02:29 -0600

    server.js fixed
    fixed bug, now renders the index & main handlebars

--- server.js
+++ server.js
@@ -4,22 +4,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const handlebars = require('express-handlebars');
+const handlebars = require("express-handlebars");
 
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
-app.engine('hbs', handlebars({
+app.engine(
+  "hbs",
+  handlebars({
     layoutsDir: `${__dirname}/views/layouts`,
-    extname: 'hbs',
-    defaultLayout: 'index',
-    partialsDir: `${__dirname}/views/partials`
-}))
+    extname: "hbs",
+    defaultLayout: "index",
+    partialsDir: `${__dirname}/views/partials`,
+  })
+);
 //Serves static files (we need it to import a css file)
 app.use(express.static("public"));
 //Sets a basic route
 app.get("/", (req, res) => {
-    res.render('main', {layout: 'index'});
-}
+  res.render("main", { layout: "index" });
 
-//Makes the app listen to port 3000
+  //Makes the app listen to port 3000
+});
 app.listen(port, () => console.log(`App listening to port ${port}`));
